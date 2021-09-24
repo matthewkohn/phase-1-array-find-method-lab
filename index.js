32f9b67e4e3c931bbb757b59121396996a0e33c6
Matthew <kohney319m@gmail.com>
2021-09-24 16:15:03 -0600

    commit tree 667adb5e11ef7948d11f385c17ea4cb8075d3263

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+// Will loop through array of records
+// Find the first object that has a win or return undefined
 
+function superbowlWin(results) {
+  let winner = results.find(item => item.result === "W");
+  if (winner !== undefined) {
+    return winner.year;
+  } else {
+    return undefined;
+  }
+}
+
+console.log(superbowlWin(record));
